4a66d6a3bac6b2616d0ea0ae9c8dc0857729d655
Mikael Svensson <github@mikaelsvensson.info>
2016-05-26 19:53:10 +0200

    Customizable naming of saved documents

--- forvaltningsrapporter/google-document-generator.php
+++ forvaltningsrapporter/google-document-generator.php
@@ -15,18 +15,28 @@ $client = createGoogleClient();
 $client->setAccessToken($_SESSION['access_token']);
 $drive_service = new Google_Service_Drive($client);
 
+$defaultParams = ['DATUM' => date('Ymd-His')];
+
 if (isset($_GET['template'])) {
     $template = $_GET['template'];
-    $exported = $drive_service->files->export("1KmrpGU8s40ZQ1RWhzXmr9Oy5OZg9rtzcaJ7XZqerguU", "application/rtf", ['alt' => 'media']);
+    $templateName = $drive_service->files->get($template)->name;
+    $exported = $drive_service->files->export($template, "application/rtf", ['alt' => 'media']);
 
-    $fileMetadata = new Google_Service_Drive_DriveFile(array(
-        'name' => 'Nytt testdokument - ' . time(),
-        'mimeType' => 'application/vnd.google-apps.document'));
+    $params = array_merge($_GET, $defaultParams);
+
     $content = $exported;//file_get_contents('files/report.csv');
-    foreach ($_GET as $key => $value) {
+    foreach ($params as $key => $value) {
         $content = str_replace($key, utf8_decode($value), $content);
     }
 
+    $newName = strtr($templateName, $params);
+    if ($newName == $templateName) {
+        // Add date to make name unique (to not replace the template with the generated file)
+        $newName .= " " . date('Ymd-His');
+    }
+    $fileMetadata = new Google_Service_Drive_DriveFile(array(
+        'name' => $newName,
+        'mimeType' => 'application/vnd.google-apps.document'));
     $file = $drive_service->files->create($fileMetadata, array(
         'data' => $content,
         'mimeType' => 'application/rtf',
@@ -56,11 +66,17 @@ if (isset($_GET['template'])) {
 <body>
 <div class="container">
     <h1>Skapa dokument</h1>
+
+    <p style="position: absolute; top: 0; right: 0; padding: 0.3em;">
+        <a href="auth-signout.php">Logga ut</a>
+    </p>
+
     <form action="google-document-generator.php" method="get" class="form-horizontal">
         <?php
         $files_list = $drive_service->files->listFiles(array("q" => "mimeType = 'application/vnd.google-apps.document'"))->getFiles();
         ?>
         <h3>Vilken mall vill du anv&auml;nda?</h3>
+
         <div class="form-group">
             <label for="template" class="col-sm-2 control-label">Mall</label>
 
@@ -91,10 +107,17 @@ if (isset($_GET['template'])) {
                 <button type="submit" class="btn btn-default">Skapa dokument</button>
             </div>
         </div>
+        <div>
+            <p>Du kan ocks&aring; anv&auml;nda dessa parametrar i ditt dokument:</p>
+            <ul>
+                <?php
+                foreach ($defaultParams as $key => $value) {
+                    printf('<li><code>%s</code>: %s</li>', $key, $value);
+                }
+                ?>
+            </ul>
+        </div>
     </form>
-    <p>
-        <a href="auth-signout.php">Logga ut</a>
-    </p>
 </div>
 </body>
 </html>
