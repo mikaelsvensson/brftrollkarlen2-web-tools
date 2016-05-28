fba66bae2ba41bc79eeed1aa08f1e713c064622c
Mikael Svensson <github@mikaelsvensson.info>
2016-05-28 11:26:22 +0200

    Pop-up overlay for document generator

---
+++ forvaltningsrapporter/renderer/BootstrapHtmlRenderer.php
@@ -0,0 +1,88 @@
+<?php
+
+//namespace report\renderer;
+
+class BootstrapHtmlRenderer
+{
+    function writerDocStart()
+    {
+    }
+
+    function write($apts)
+    {
+        print <<< HTML_START
+        <table class="table table-striped table-hover table-condensed">
+        <thead>
+        <tr>
+HTML_START;
+
+        $headers = array();
+        foreach ($apts as $apt) {
+            $headers = array_merge($headers, array_keys($apt));
+        }
+        $headers = array_unique($headers);
+
+        $stats = [];
+        foreach ($headers as $header) {
+            print "<th>$header</th>";
+        }
+
+        print <<< HTML_START
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+HTML_START;
+        foreach ($apts as $apt) {
+            foreach ($headers as $header) {
+                $value = join(',', $apt[$header]);
+                $stats[$header][$value]++;
+            }
+        }
+
+        foreach ($headers as $header) {
+            print '<td><ul class="list-unstyled">';
+            arsort($stats[$header]);
+            $slice = count($stats[$header]) > 10;
+            if ($slice) {
+                $stats[$header] = array_slice($stats[$header], 0, 9);
+            }
+            foreach ($stats[$header] as $value => $count) {
+                printf('<li><small>%s: %s</small></li>', $value, $count);
+            }
+            if ($slice) {
+                print '<li><small>...</small></li>';
+            }
+            print "</ul></td>";
+        }
+
+        print <<< HTML_START
+        </tr>
+        </tfoot>
+        <tbody>
+HTML_START;
+
+
+        foreach ($apts as $apt) {
+            print '<tr class="entry">';
+            foreach ($headers as $header) {
+                $value = join(',', $apt[$header]);
+                printf('<td class="%s">%s</td>', $header, $value);
+            }
+            print "</tr>";
+        }
+
+        print <<< HTML_END
+        </tbody>
+        </table>
+HTML_END;
+    }
+
+    function writerDocEnd()
+    {
+        print <<< HTML_END
+        </body>
+        </html>
+HTML_END;
+    }
+}
--- forvaltningsrapporter/template-links.js
+++ forvaltningsrapporter/template-links.js
@@ -1,18 +1,55 @@
 (function () {
     $(document).ready(function () {
-        $("table").each (function (i, table) {
+
+        var toggleDialog = function () {
+            $(document.body).toggleClass("overlay-backdrop");
+            $("#generator").toggleClass("hidden");
+        };
+
+        $("#button-close").click(toggleDialog);
+
+        $("table").each(function (i, table) {
             var headers = $(table).children("thead").children("tr").children("th").map(function (index, th) {
                 return $(th).text();
             });
 
             $(table).children("tbody").children("tr.entry").each(function (index, tr) {
                 var cells = $(tr).children("td");
-                var firstCell = cells[0];
-                var values = cells.map(function (x, td) {
-                    return headers[x].toUpperCase() + "=" + encodeURIComponent(td.innerHTML);
-                }).get().join("&");
 
-                cells.first().prepend($("<a>").text("[dok]").attr("href", "google-document-generator.php?" + values));
+                cells.first().prepend($("<a>").text("[dok]").attr("href", "#").click(function (e) {
+
+                    var paramContainer = $("#parameters");
+                    paramContainer.empty();
+
+                    var cells = $(this).parent().parent().children("td");
+                    for (var x = 0; x < cells.length; x++) {
+                        var td = cells[x];
+
+                        var paramName = headers[x].toUpperCase();
+                        var paramValue = td.innerText;
+
+                        paramContainer.append(
+                            $("<div/>")
+                                .addClass("form-group")
+                                .append($("<label/>")
+                                    .text(paramName)
+                                    .attr("for", "field-" + paramName)
+                                    .addClass("col-sm-4 control-label"))
+
+                                .append($("<div/>")
+                                    .addClass("col-sm-8")
+                                    .append($("<input/>")
+                                        .attr("type", "text")
+                                        .attr("name", paramName)
+                                        .attr("id", "field-" + paramName)
+                                        .val(paramValue)
+                                        .addClass("form-control"))
+                            )
+                        );
+                    }
+
+                    toggleDialog();
+                }));
             });
         });
     });
