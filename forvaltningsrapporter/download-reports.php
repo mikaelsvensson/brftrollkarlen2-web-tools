a74c2c8a5b1bdf641c9ed3266a82feced7c2b99f
Mikael Svensson <mikael.svensson@klarna.com>
2018-01-27 13:34:14 +0100

    Removed to-do item

--- forvaltningsrapporter/download-reports.php
+++ forvaltningsrapporter/download-reports.php
@@ -9,8 +9,6 @@ require_once 'ReportReader.php';
 require_once 'config.php';
 require_once '../lib/PdfParser.php';
 
-//TODO: How is the execution of this script scheduled since One.com does not provide a scheduler (right?). Are we using some king of public "web ping service" pointed to this script? Maybe IFTTT.com?
-
 $cookie = null;
 
 function getAuthCookie($stofastUser, $stofastPassword)
